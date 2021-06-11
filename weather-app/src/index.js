9ab122418be06dc2ee938e9230e9bc4114e8e16d
BMWire <bmwemawire@gmail.com>
2021-06-11 12:52:45 +0300

    Create .env file and hide it from the repo

--- weather-app/src/index.js
+++ weather-app/src/index.js
@@ -1,5 +1,8 @@
+/* eslint-disable import/no-named-as-default */
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable no-undef */
 import React from 'react';
-import { ReactDOM } from 'react-dom';
+import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './App';
@@ -9,7 +12,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  // document.getElementById('root'),
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
